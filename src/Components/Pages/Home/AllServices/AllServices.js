bf3a2d69c985a03fe7f2c5a4afdcf5278ae491c3
siam-26 <siamshahriar390@gmail.com>
2022-11-08 15:52:41 +0600

    services and footer section added

---
+++ src/Components/Pages/Home/AllServices/AllServices.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './AllServices';
+
+const AllServices = () => {
+    return (
+        <div>
+            <h1 className='text-center text-4xl mt-20'>Our All Services</h1>
+            <div className='services-div grid grid-cols-3'>
+
+                <div className="card w-96 bg-base-100 gh">
+
+                    <figure className="px-10 pt-10">
+                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="w-60" />
+                    </figure>
+                    <div className="card-body items-center text-center body">
+                        <h2 className="card-title name">Chicken Burger</h2>
+                        <h3 className='price font-bold'>89.00</h3>
+                        <p className='short-description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed molestias quibusdam distinctio qui exercitationem consequuntur suscipit veritatis, sint ea ipsam, perspiciatis cumque architecto laborum! Ut excepturi, aliquid magni dolore, quibusdam blanditiis quasi eaque maiores, cum non fuga aut quas veritatis porro asperiores corrupti ducimus saepe error maxime nostrum consectetur! Provident quae nisi similique in distinctio earum beatae doloremque quaerat voluptatum, voluptate molestiae labore iste ea, dicta hic dignissimos tenetur eos tempore delectus autem soluta magnam quisquam. Similique repellat neque voluptatibus, minus delectus dolorem quibusdam pariatur ipsa doloremque nisi natus quasi soluta animi placeat! Adipisci incidunt nulla vero magni blanditiis assumenda!</p>
+
+                    </div>
+                    <Link to='/allservices'>
+                        <button className="seeAll-btn font-bold mt-8">See All</button>
+                    </Link>
+
+                </div>
+
+                <div className="card w-96 bg-base-100 gh">
+
+                    <figure className="px-10 pt-10">
+                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="w-60" />
+                    </figure>
+                    <div className="card-body items-center text-center body">
+                        <h2 className="card-title name">Chicken Burger</h2>
+                        <h3 className='price font-bold'>89.00</h3>
+                        <p className='short-description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed molestias quibusdam distinctio qui exercitationem consequuntur suscipit veritatis, sint ea ipsam, perspiciatis cumque architecto laborum! Ut excepturi, aliquid magni dolore, quibusdam blanditiis quasi eaque maiores, cum non fuga aut quas veritatis porro asperiores corrupti ducimus saepe error maxime nostrum consectetur! Provident quae nisi similique in distinctio earum beatae doloremque quaerat voluptatum, voluptate molestiae labore iste ea, dicta hic dignissimos tenetur eos tempore delectus autem soluta magnam quisquam. Similique repellat neque voluptatibus, minus delectus dolorem quibusdam pariatur ipsa doloremque nisi natus quasi soluta animi placeat! Adipisci incidunt nulla vero magni blanditiis assumenda!</p>
+
+                    </div>
+                </div>
+
+                <div className="card w-96 bg-base-100 gh">
+
+                    <figure className="px-10 pt-10">
+                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="w-60" />
+                    </figure>
+                    <div className="card-body items-center text-center body">
+                        <h2 className="card-title name">Chicken Burger</h2>
+                        <h3 className='price font-bold'>89.00</h3>
+                        <p className='short-description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed molestias quibusdam distinctio qui exercitationem consequuntur suscipit veritatis, sint ea ipsam, perspiciatis cumque architecto laborum! Ut excepturi, aliquid magni dolore, quibusdam blanditiis quasi eaque maiores, cum non fuga aut quas veritatis porro asperiores corrupti ducimus saepe error maxime nostrum consectetur! Provident quae nisi similique in distinctio earum beatae doloremque quaerat voluptatum, voluptate molestiae labore iste ea, dicta hic dignissimos tenetur eos tempore delectus autem soluta magnam quisquam. Similique repellat neque voluptatibus, minus delectus dolorem quibusdam pariatur ipsa doloremque nisi natus quasi soluta animi placeat! Adipisci incidunt nulla vero magni blanditiis assumenda!</p>
+
+                    </div>
+                </div>
+
+                <div className="card w-96 bg-base-100 gh">
+
+                    <figure className="px-10 pt-10">
+                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="w-60" />
+                    </figure>
+                    <div className="card-body items-center text-center body">
+                        <h2 className="card-title name">Chicken Burger</h2>
+                        <h3 className='price font-bold'>89.00</h3>
+                        <p className='short-description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed molestias quibusdam distinctio qui exercitationem consequuntur suscipit veritatis, sint ea ipsam, perspiciatis cumque architecto laborum! Ut excepturi, aliquid magni dolore, quibusdam blanditiis quasi eaque maiores, cum non fuga aut quas veritatis porro asperiores corrupti ducimus saepe error maxime nostrum consectetur! Provident quae nisi similique in distinctio earum beatae doloremque quaerat voluptatum, voluptate molestiae labore iste ea, dicta hic dignissimos tenetur eos tempore delectus autem soluta magnam quisquam. Similique repellat neque voluptatibus, minus delectus dolorem quibusdam pariatur ipsa doloremque nisi natus quasi soluta animi placeat! Adipisci incidunt nulla vero magni blanditiis assumenda!</p>
+
+                    </div>
+                </div>
+
+
+
+                <div className="card w-96 bg-base-100">
+
+                    <figure className="px-10 pt-10">
+                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="w-60" />
+                    </figure>
+                    <div className="card-body items-center text-center body">
+                        <h2 className="card-title name">Chicken Burger</h2>
+                        <h3 className='price font-bold'>89.00</h3>
+                        <p className='short-description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed molestias quibusdam distinctio qui exercitationem consequuntur suscipit veritatis, sint ea ipsam, perspiciatis cumque architecto laborum! Ut excepturi, aliquid magni dolore, quibusdam blanditiis quasi eaque maiores, cum non fuga aut quas veritatis porro asperiores corrupti ducimus saepe error maxime nostrum consectetur! Provident quae nisi similique in distinctio earum beatae doloremque quaerat voluptatum, voluptate molestiae labore iste ea, dicta hic dignissimos tenetur eos tempore delectus autem soluta magnam quisquam. Similique repellat neque voluptatibus, minus delectus dolorem quibusdam pariatur ipsa doloremque nisi natus quasi soluta animi placeat! Adipisci incidunt nulla vero magni blanditiis assumenda!</p>
+
+                    </div>
+                </div>
+
+                <div className="card w-96 bg-base-100 gh">
+
+                    <figure className="px-10 pt-10">
+                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="w-60" />
+                    </figure>
+                    <div className="card-body items-center text-center body">
+                        <h2 className="card-title name">Chicken Burger</h2>
+                        <h3 className='price font-bold'>89.00</h3>
+                        <p className='short-description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed molestias quibusdam distinctio qui exercitationem consequuntur suscipit veritatis, sint ea ipsam, perspiciatis cumque architecto laborum! Ut excepturi, aliquid magni dolore, quibusdam blanditiis quasi eaque maiores, cum non fuga aut quas veritatis porro asperiores corrupti ducimus saepe error maxime nostrum consectetur! Provident quae nisi similique in distinctio earum beatae doloremque quaerat voluptatum, voluptate molestiae labore iste ea, dicta hic dignissimos tenetur eos tempore delectus autem soluta magnam quisquam. Similique repellat neque voluptatibus, minus delectus dolorem quibusdam pariatur ipsa doloremque nisi natus quasi soluta animi placeat! Adipisci incidunt nulla vero magni blanditiis assumenda!</p>
+
+                    </div>
+                </div>
+            </div>
+            {/* <div className='btn-div'>
+                <Link to='/allservices'>
+                    <button className="seeAll-btn font-bold mt-8">See All</button>
+                </Link>
+            </div> */}
+
+        </div>
+    );
+};
+
+export default AllServices;
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.css';
 import banner from '../../Assets/Images/Banner/banner.png'
+import HomeServicesShortDetails from './HomeServicesShortDetails/HomeServicesShortDetails';
 
 const Home = () => {
     return (
         <div>
+
             <div className='header grid grid-cols-2'>
                 <div className='home-header-content text-justify mt-28 pl-20'>
                     <h1 className='text-6xl font-bold'>The Best</h1>
@@ -13,6 +15,10 @@ const Home = () => {
                     <p className='font-light text-2xl mt-4'>Send unique gift to anywhere in Bangladesh form our 'HMAS-Food' collection.</p>
                 </div>
                 <img src={banner} alt="" />
+            </div>
+
+            <div>
+                <HomeServicesShortDetails></HomeServicesShortDetails>
             </div>
         </div>
     );
