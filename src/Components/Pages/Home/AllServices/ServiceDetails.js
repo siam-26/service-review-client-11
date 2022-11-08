f1bec31acdae716c9f2ddb871cb046f102a7aefb
siam-26 <siamshahriar390@gmail.com>
2022-11-08 22:27:33 +0600

    people's review section added

--- src/Components/Pages/Home/AllServices/ServiceDetails.js
+++ src/Components/Pages/Home/AllServices/ServiceDetails.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Review from './Review';
 import './serviceDetails.css'
 
 const ServiceDetails = () => {
     return (
-        <div className="card w-96 bg-base-100 w-full">
+        <div className="card bg-base-100 w-full">
             <h1 className="mb-8 mt-12 text-5xl font-bold text-center serviceDetails-h">Chicken Burger</h1>
 
             <div>
@@ -26,11 +26,17 @@ const ServiceDetails = () => {
                 <h3 className='price font-bold'>Price: 89.00tk</h3>
                 <p className='serviceDetails-description font-semibold'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed molestias quibusdam distinctio qui exercitationem consequuntur suscipit veritatis, sint ea ipsam, perspiciatis cumque architecto laborum! Ut excepturi, aliquid magni dolore, quibusdam blanditiis quasi eaque maiores, cum non fuga aut quas veritatis porro asperiores corrupti ducimus saepe error maxime nostrum consectetur! Provident quae nisi similique in distinctio earum beatae doloremque quaerat voluptatum, voluptate molestiae labore iste ea, dicta hic dignissimos tenetur eos tempore delectus autem soluta magnam quisquam. Similique repellat neque voluptatibus, minus delectus dolorem quibusdam pariatur ipsa doloremque nisi natus quasi soluta animi placeat! Adipisci incidunt nulla vero magni blanditiis assumenda!</p>
 
-                <div>
+                <h1 className='mt-12 mb-12 text-5xl font-bold review-heading'>People's Review</h1>
+                <div className='review-container h-auto w-full'>
+                    <Review></Review>
+                </div>
+
+
+                {/* <div>
                     <Link to='/serviceDetails'>
                         <button className="allServices-btn font-bold mt-8">view details</button>
                     </Link>
-                </div>
+                </div> */}
             </div>
 
 
